4fb70e54a2f9312821fba3bcfbc4dda72385aad3
emirhanusta <emirhan1usta@gmail.com>
2025-03-07 15:44:21 +0300

    initial commit

---
+++ src/test/java/patika/defineX/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package patika.defineX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
