3595938d0ed3ad40cabd33b955552a539dc3774c
emirhanusta <emirhan1usta@gmail.com>
2025-03-23 14:22:14 +0300

    update redis ttl value

--- src/main/java/patika/defineX/config/RedisConfig.java
+++ src/main/java/patika/defineX/config/RedisConfig.java
@@ -1,16 +1,24 @@
 package patika.defineX.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
+import java.time.Duration;
+
 @EnableCaching
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.cache.redis.time-to-live}")
+    private long ttl;
+
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig();
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMillis(ttl))
+                .disableCachingNullValues();
     }
 }
